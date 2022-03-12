9c1b01ae9d8c1f08256a517432a0665b8f2ded26
Scott Howard <scott@showard.ca>
2022-03-12 14:23:46 -0800

    fixed issue with num_fmaps not matching ConvPass argument

--- 02_train/utils/mgmodel.py
+++ 02_train/utils/mgmodel.py
@@ -2,10 +2,11 @@ import torch
 from funlib.learn.torch.models import UNet, ConvPass
 
 def initModel():
+    num_fmaps = 8
     # Define the UNet
     unet = UNet(
         in_channels=3,
-        num_fmaps=8, # tutorial used 4
+        num_fmaps=num_fmaps, # tutorial used 4
         fmap_inc_factor=2,
         downsample_factors=[[2,2], [2,2]],
         kernel_size_down=[[[3,3], [3,3]]]*3,
@@ -15,7 +16,7 @@ def initModel():
     # Define the model
     model = torch.nn.Sequential(
         unet,
-        ConvPass(in_channels=4,
+        ConvPass(in_channels=num_fmaps,
                  out_channels=1,
                  kernel_sizes=[(1,1)], # 2 dimensions => Conv2d
                  activation=None),
