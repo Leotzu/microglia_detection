db07d2112eaaea8bd8521f64714dd9508b1b55ed
Elliot Lupini <30415913+elupini@users.noreply.github.com>
2022-03-13 22:51:05 -0700

    Update score.py
    Added scoring function based on distance between centroid of objects

--- 02_train/utils/score.py
+++ 02_train/utils/score.py
@@ -25,3 +25,68 @@ def count_xor_difference(arr1, arr2, threshold=0.7):
 
     return np.count_nonzero(np.logical_xor(arr1 > threshold,
                                            arr2 > threshold))
+
+# Computes the distances between the centroids of the predicted
+# objects to the centroids of the actual objects
+# Returns a list of distances
+def compute_centroids(seg, pred, pixel_threshold=0.7, size_threshold=1):
+    
+    seg_coordinates_dict = {}
+    pred_coordinates_dict = {}
+    
+    seg_centroid_dict = {}
+    pred_centroid_dict = {}
+    
+    # Predicted Objects
+    pred[pred > pixel_threshold] = 1
+    pred[pred <= pixel_threshold] = 0
+    
+    label, num_label = ndimage.label(pred == 1)
+    size = np.bincount(label.ravel())
+    num_objects = (size > size_threshold).sum()
+    
+    for i in range(1,len(size)):
+        pairs_pred = np.asarray(np.where(label == i)).T
+        pred_coordinates_dict[i] = pairs_pred
+        
+    for key,value in pred_coordinates_dict.items():
+        x2 = [p[0] for p in value]
+        y2 = [p[1] for p in value]
+        centroid_pred = (sum(x2) / len(value), sum(y2) / len(value))
+        pred_centroid_dict[key] = centroid_pred
+        
+    # Seg Objects
+    seg[seg > pixel_threshold] = 1
+    seg[seg <= pixel_threshold] = 0
+    
+    label, num_label = ndimage.label(seg == 1)
+    size = np.bincount(label.ravel())
+    num_objects = (size > size_threshold).sum() 
+        
+    for i in range(1,len(size)):
+        pairs_seg = np.asarray(np.where(label == i)).T
+        seg_coordinates_dict[i] = pairs_seg
+        
+    for key,value in seg_coordinates_dict.items():
+        x2 = [p[0] for p in value]
+        y2 = [p[1] for p in value]
+        centroid_seg = (sum(x2) / len(value), sum(y2) / len(value))
+        seg_centroid_dict[key] = centroid_seg
+    
+    dists = []
+
+    for pkey, pvalue in pred_centroid_dict.items():
+        
+        for skey, svalue in seg_centroid_dict.items():
+            
+            tmp_dist_list = []
+            dist = np.sqrt((pvalue[0] - svalue[0])**2 + (pvalue[1] - svalue[1])**2)
+            tmp_dist_list.append(dist)
+        
+        dists.append(np.asarray(tmp_dist_list).min())
+    
+    if(len(seg_centroid_dict)!=len(pred_centroid_dict)):
+            
+        dists.remove(np.asarray(dists).max())
+        
+    return dists
