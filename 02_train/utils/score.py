8d9c16d3f25a288d22f7f6b4157a368ca9a854ea
Scott Howard <scott@showard.ca>
2022-03-13 18:27:26 -0700

    added a function to count the number of pixels differing between two arrays within a threshold

--- 02_train/utils/score.py
+++ 02_train/utils/score.py
@@ -14,3 +14,14 @@ def count_objects(data, pixel_threshold=0.7, size_threshold=1):
     num_objects = (size > size_threshold).sum()
     
     return num_objects
+
+def count_xor_difference(arr1, arr2, threshold=0.7):
+    """Count the number of pixels that differ between two numpy arrays.
+
+    arr1,arr2 -- arrays to compare
+    threshold -- array elements above this threshold will be considered
+                 'on', and 'off' if below or equal to.
+    """
+
+    return np.count_nonzero(np.logical_xor(arr1 > threshold,
+                                           arr2 > threshold))
